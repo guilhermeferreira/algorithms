11659f61607a79247be74778b6878054ca358f5b
gmacielferreira <gmacielferreira@LUX-GDL-005>
2017-03-01 17:02:34 -0600

    Add project files.

---
+++ week1/week1.cpp
@@ -0,0 +1,14 @@
+// week1.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Week 1" << endl;
+
+    return 0;
+}
+
